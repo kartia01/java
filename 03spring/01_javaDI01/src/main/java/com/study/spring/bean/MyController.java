02d8428518e4b7662dae100324972b89e48a442f
kartia01 <kartia25142@gmail.com>
2025-10-15 18:10:54 +0900

    spring02

--- 03spring/01_javaDI01/src/main/java/com/study/spring/bean/MyController.java
+++ 03spring/01_javaDI01/src/main/java/com/study/spring/bean/MyController.java
@@ -1,5 +1,7 @@
 package com.study.spring.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,8 +9,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MyController {
 	
-	@RequestMapping("/lim")
+	@Autowired
+	Member member1;
+	
+	@Autowired
+	@Qualifier("printerB")
+	Printer printer;
+	
+	@RequestMapping("/")
 	public @ResponseBody  String root() {
+		member1.print();
+		
+		member1.setPrinter(printer);
+		member1.print();
+		
 		return "안녕하세요";
 	}
 }
